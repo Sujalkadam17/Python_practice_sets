b005a7fd6fc169f7c8a4d98de39099f5c753055f
Sujalkadam17 <sujalkadam1711@gmail.com>
2025-03-11 18:53:46 +0530

    Added text_to_speech.py file

--- jokes.py
+++
@@ -1,3 +0,0 @@
-import pyjokes as pj
-joke = pj.get_joke()
-print(joke)
---
+++ text_to_speech.py
@@ -0,0 +1,4 @@
+import pyttsx3 
+engine = pyttsx3.init()
+engine.say("Hey there Welcome to Sujal's python repository")
+engine.runAndWait()
