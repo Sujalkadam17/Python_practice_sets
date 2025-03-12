526812124c23d3be00cff7d92866f396e5f5e50c
Sujal Kadam <162724689+Sujalkadam17@users.noreply.github.com>
2025-03-12 10:49:49 +0530

    added files

---
+++ python_logic_building.py/left_triangle_patterns.py
@@ -0,0 +1,11 @@
+#printing star pattern
+for i in range(5):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+
+#printing number 
+for i in range(5):
+    for j in range(i):
+        print(i,end=" ")
+    print()
---
+++ python_logic_building.py/palindrome.py
@@ -0,0 +1,2 @@
+n = input("Enter a number :")
+print("palindrome" if n == n[::-1] else "Not palindrome") 
