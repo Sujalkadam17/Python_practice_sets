49f3fadf0849ddb0dc364c7625332b6817546250
Sujal Kadam <162724689+Sujalkadam17@users.noreply.github.com>
2025-05-02 18:46:52 +0530

    Add files via upload

---
+++ python_logic_building.py/6b_date.py
@@ -0,0 +1,8 @@
+import datetime as dt
+
+print("Current date and time: ", dt.datetime.now())
+print("Current year: ", dt.datetime.now().year)
+print("Current month: ", dt.datetime.now().month)
+print("Current date: ", dt.datetime.now().date())
+print("Current time: ", dt.datetime.now().time())
+
