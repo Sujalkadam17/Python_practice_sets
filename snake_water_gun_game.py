216e7fa492b66280f2416b78b2ef8575d610da87
Sujal Kadam <162724689+Sujalkadam17@users.noreply.github.com>
2025-05-06 19:17:02 +0530

    Update snake_water_gun_game.py

--- snake_water_gun_game.py
+++ snake_water_gun_game.py
@@ -13,3 +13,25 @@ info = {
     'w': 'Water 🌊',
     'g': 'Gun 🔫'
 }
+
+while True:
+    print("\n🆕 Round!\n")
+    computer = random.choice(['s', 'w', 'g'])
+    print("Enter 's' for snake🐍, 'w' for water🌊, 'g' for gun🔫")
+    user = input("\nEnter your choice: ").lower() #lower() to handle case sensitivity
+    print("\nYour choice:", info[user])
+    print("Computer choice:", info[computer])
+
+    if user == computer:
+        print("It's a Draw !")
+    else:
+        if (user == 's' and computer == 'w') or (user == 'w' and computer == 'g') or (user == 'g' and computer == 's'):
+            print(" You Won ! 🥳")
+        elif (user == 's' and computer == 'g') or (user == 'w' and computer == 's') or (user == 'g' and computer == 'w'):
+            print(" You Lose ! 😢")
+        else:
+            print("Invalid input! Please enter 's', 'w', or 'g'.")
+    user = input("Press Enter to play again or type 'exit' to quit: ").lower()
+    if user == 'exit':
+        print("Thanks for playing!")
+        break
