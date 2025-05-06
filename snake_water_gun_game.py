27e99ca2cc433eab31ed005dd67a2fe2aded4b62
Sujal Kadam <162724689+Sujalkadam17@users.noreply.github.com>
2025-05-06 19:16:44 +0530

    Update snake_water_gun_game.py

--- snake_water_gun_game.py
+++ snake_water_gun_game.py
@@ -1,1 +1,15 @@
+#Snake water gun game
+import random # to generate random choices for the computer
 
+print("Welcome to Snake🐍 Water🌊 Gun🔫 Game!")
+print("📜Rules:")
+print("1️⃣ Snake🐍 beats Water🌊")
+print("2️⃣ Water🌊 beats Gun🔫")
+print("3️⃣ Gun🔫 beats Snake🐍")
+start = input("🟢Press Enter to start the game...")
+
+info = {
+    's': 'Snake 🐍',
+    'w': 'Water 🌊',
+    'g': 'Gun 🔫'
+}
