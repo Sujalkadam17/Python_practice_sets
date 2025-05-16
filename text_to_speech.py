3b2fafb3d05294f95a0690a4365a2368482db7fd
Sujal Kadam <162724689+Sujalkadam17@users.noreply.github.com>
2025-05-16 11:37:02 +0530

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,4 +1,14 @@
 import pyttsx3 
 engine = pyttsx3.init()
-engine.say("Hey there Welcome to Sujal's python repository")
+engine.say("Welcome Buddy... Type here To Read Aloud")
 engine.runAndWait()
+
+while True:
+    engine.setProperty('rate', 150)  # Speed of speech
+    text = input("Type here To Read Aloud : ")
+    if text == "exit":
+        break
+
+    print("Type 'exit' to stop the program")
+    engine.say(text)
+    engine.runAndWait()
