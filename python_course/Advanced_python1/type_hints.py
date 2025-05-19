5d45823aa4b741bd0ec2c7f851bd06beb83b089b
Sujal Kadam <162724689+Sujalkadam17@users.noreply.github.com>
2025-05-19 15:35:55 +0530

    Add files via upload

---
+++ python_course/Advanced_python1/type_hints.py
@@ -0,0 +1,10 @@
+from typing import List, Dict, Tuple, Union
+
+lst :List[int] = [1, 2, 3, 4, 5]
+
+tup : tuple[str,int] = ("Leo", 7)
+
+d : dict[str,int] = {"Leo": 7, "Sujal": 8}
+
+print(tup)
+print(d)
