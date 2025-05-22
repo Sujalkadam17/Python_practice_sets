f5d64289f1a94629ea1da5a8c0c6b717336069e2
Sujal Kadam <162724689+Sujalkadam17@users.noreply.github.com>
2025-05-22 18:34:10 +0530

    Add files via upload

---
+++ python_course/Advanced_python1/format_str.py
@@ -0,0 +1,6 @@
+
+name =input("Enter your name: ")
+mks =input("Enter your marks: ")
+Ph_No =input("Enter your phone number: ")
+
+print("The name of the student is {}, His marks are {} and phone number is {}".format(name,mks,Ph_No))
